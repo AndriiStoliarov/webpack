f30915c7a10b6fccdd5e523ac2bcc78933a6cb52
Andrii Stoliarov <andrey159852@gmail.com>
2023-11-13 16:41:36 +0200

    file updated 13112023

--- methed/webpack-course-2022/src/index.js
+++ methed/webpack-course-2022/src/index.js
@@ -1,5 +1,13 @@
 import './index.html';
+import './index.scss';
+import code from './img/code.png';
+import { mult, sum } from './modules/calc';
 
-const mult = (a, b) => a * b;
+const imgWrap = document.querySelector('.img');
+const img = new Image();
+img.src = code;
+img.width = 700;
+imgWrap.append(img);
 
-console.log(mult(3, 4));
+console.log('mult :>>', mult(3, 4));
+console.log('sum :>> ', sum(3, 4));
